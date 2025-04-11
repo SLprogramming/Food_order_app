413ba0e8bb27760275ec6b3361024951839fc869
TaingLinnMaung <joesat2516@gmail.com>
2025-04-11 11:25:25 +0630

    save

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import NavBar from "./components/NavBar";
 import ClickSpark from "./reactbits/ClickSpark.jsx";
 import Banner from "./components/Banner.jsx";
+import PopularItems from "./components/PopularItems.jsx";
 
 function App() {
   const backgroundImage = {
     backgroundImage: "url('/public/images/slider-eclipse.png')",
     height: "100vh",
-    width: "100vw",
 
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
@@ -17,15 +17,18 @@ function App() {
   return (
     <>
       <ClickSpark
+      className=" "
         sparkColor="#fff"
         sparkSize={10}
         sparkRadius={15}
         sparkCount={8}
         duration={400}
       >
-        <div style={backgroundImage} className=" w-full bg-gray-100">
+        <div style={backgroundImage} className=" w-screen  bg-gray-100">
           <NavBar />
           <Banner />
+          
+          <PopularItems/>
         </div>
         {/* Your content here */}
       </ClickSpark>
--- src/components/PopularItems.jsx
+++ src/components/PopularItems.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
+
+
 const PopularItems = () => {
   return (
-    <div>PopularItems</div>
+   
+
+    <div className=''>
+      <div className='text-center font-bold text-[var(--color-primary)]'>Quick pick</div>
+      <h1 className='text-6xl font-[700] text-center text-[#202020]'>Popular Goods</h1>
+    </div>
+
   )
 }
 
